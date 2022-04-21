3f208dcec8de20ae05ca3941b5340e54eaed948d
Goanna7007 <45587025+Goanna7007@users.noreply.github.com>
2022-04-21 13:30:24 +1000

    List view for history now working

--- app/src/main/java/au/edu/utas/joeyn/strokerehab/ui/history/HistoryFragment.kt
+++ app/src/main/java/au/edu/utas/joeyn/strokerehab/ui/history/HistoryFragment.kt
@@ -6,7 +6,10 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import au.edu.utas.joeyn.strokerehab.databinding.FragmentHistoryBinding
+import au.edu.utas.joeyn.strokerehab.databinding.ListViewItemThreeTextBinding
 
 class HistoryFragment : Fragment() {
 
@@ -15,6 +18,15 @@ class HistoryFragment : Fragment() {
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
+
+    val items = mutableListOf<String>(
+        "Rick",
+        "Morty",
+        "Beth",
+        "Summer",
+        "Not Jerry"
+    )
+
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -26,13 +38,36 @@ class HistoryFragment : Fragment() {
         _binding = FragmentHistoryBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textDashboard
-        textView.text = "History is working"
-        return root
+
+
+        binding.historyList.adapter = HistoryItemAdapter(historyItems = items)
+        binding.historyList.layoutManager = LinearLayoutManager(binding.root.context)
+
+        return binding.root
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+
+
+    inner class ItemView(var ui: ListViewItemThreeTextBinding) : RecyclerView.ViewHolder(ui.root) {}
+
+    inner class HistoryItemAdapter(private val historyItems: MutableList<String>) : RecyclerView.Adapter<ItemView>() {
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemView {
+            val ui = ListViewItemThreeTextBinding.inflate(layoutInflater, parent, false)   //inflate a new row from the my_list_item.xml
+            return ItemView(ui)
+        }
+
+        override fun onBindViewHolder(holder: ItemView, position: Int) {
+            val tx = historyItems[position]   //get the data at the requested position
+            holder.ui.leftText.text = tx //set the TextView in the row we are recycling
+        }
+
+        override fun getItemCount(): Int {
+            return historyItems.size
+        }
     }
 }
